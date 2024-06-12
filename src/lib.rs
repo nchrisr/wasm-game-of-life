56430b58c962b2a38ba50866b1d495d7344d4c31
nchrisr <24297785+nchrisr@users.noreply.github.com>
2024-06-11 22:43:45 -0500

    Added setters and updated html

--- src/lib.rs
+++ src/lib.rs
@@ -75,9 +75,26 @@ impl Universe {
         self.height
     }
 
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+    }
+
     pub fn get_cells(&self) -> *const Cell {
         self.cells.as_ptr()
     }
+
+    /*pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }*/
 
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
