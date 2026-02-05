4a2b929b7995c178b26782a07ce12e4f700d2f58
Rodney <24297785+nchrisr@users.noreply.github.com>
2026-02-04 21:35:05 -0500

    fix build and run issues

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -6,9 +6,25 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
+    publicPath: "/",
   },
   mode: "development",
+  experiments: {
+    asyncWebAssembly: true
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080,
+    hot: true,
+  },
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'index.html' }
+      ]
+    })
   ],
 };
